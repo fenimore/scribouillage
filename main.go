ca0b6746467010cedea24e63d23c8a2e71511c83
polypmer <exorable.ludos@gmail.com>
2016-08-30 20:38:59 -0400

    starting to understand bits of bytes

--- main.go
+++ main.go
@@ -4,12 +4,14 @@
 //                                       Product id = 00ff
 // My infinity pedal outputs to a file in /dev/usb hiddev0
 // This, however, could change (to say, hiddev1?)
+// Notes on fmt Formating, %b is base 2 %o is base 8.
+// Looks like the data is given in unsigned base 8 ints. uint8
+// From 24 byte buffer, bytes 5, 13, and 21 are important.
+
 package main
 
 import "os"
 import "fmt"
-
-// import "reflect"
 
 func main() {
 	file, err := os.Open("/dev/usb/hiddev0")
@@ -22,9 +24,7 @@ func main() {
 		if err != nil {
 			fmt.Println(err)
 		}
-		fmt.Printf("read %d bytes: %q\n\n", count, data[:count])
-		//fmt.Printf("%d", reflect.ValueOf(data[2]))
-		// TODO: should I use %b, %c or %o or %q
+		//fmt.Printf("read %d bytes: %q\n\n", count, data[:count])
 		fmt.Printf("Left %b, Center %b, Right %b", data[4], data[12], data[20])
 	}
 }
