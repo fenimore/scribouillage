ab39e9cdfa7edd35426099942ca9db57cf8975cf
polypmer <exorable.ludos@gmail.com>
2016-08-30 20:34:38 -0400

    reading input.. mmmh

--- main.go
+++ main.go
@@ -1,3 +1,9 @@
+// Fenimore Love | Public Domain
+// See Python2 (yuck) driver https://ubuntuforums.org/showthread.php?t=2232673
+// Also, some info about Infinity pedal: Vendor id = 05f3
+//                                       Product id = 00ff
+// My infinity pedal outputs to a file in /dev/usb hiddev0
+// This, however, could change (to say, hiddev1?)
 package main
 
 import "os"
@@ -10,13 +16,15 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	data := make([]byte, 100)
+	data := make([]byte, 24)
 	for {
 		count, err := file.Read(data)
 		if err != nil {
 			fmt.Println(err)
 		}
 		fmt.Printf("read %d bytes: %q\n\n", count, data[:count])
-		fmt.Printf("%d", reflect.ValueOf(data[2]))
+		//fmt.Printf("%d", reflect.ValueOf(data[2]))
+		// TODO: should I use %b, %c or %o or %q
+		fmt.Printf("Left %b, Center %b, Right %b", data[4], data[12], data[20])
 	}
 }
