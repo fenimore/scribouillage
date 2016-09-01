4cf174224628fb62a41f1d1116f43537e197b9a0
polypmer <exorable.ludos@gmail.com>
2016-08-31 21:28:36 -0400

    readme

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # pedal-driver
 
-Not working yet.
+So this works for taking an **infinity foot pedal** (or others?). It defaults with maps to the buttons Previous, Play, and Next. This is for Linux and relies on a Linux utility `xte` (for Arch users, check out the xautomation package).
+
+This driver, if it can be called that (it's quite simple, and hacky, and it isn't really a driver...), is in the Public Domain. It's more of a pedal input mapper using `xte`...
+
+It can either be run using sudo, or it a user can be added to a group with privilages to read `/dev/usb/hiddev0`.
+
+For **transcription**, the pedals ought to *just work* for playing, skipping, and returning on VLC (or other music players). So, for transcription, the default VLC hotkeys (and these can be set in the Preferences) are `Shift+Left/Alt+Left/Shift+Alt+Left` and `Shift+Right/Alt+Right/Shift+Alt+Right` for **short/medium/long jumps forward and backwards**, respectively.
--- main.go
+++ main.go
@@ -1,20 +1,23 @@
-// Fenimore Love | Public Domain
-// See Python2 driver https://ubuntuforums.org/
+// Public Domain | Fenimore Love
+//
+// Inspired by Python2 driver https://ubuntuforums.org/
 //                           showthread.php?t=2232673
+//
 // Also, some info about Infinity pedal: Vendor id = 05f3
 //                                       Product id = 00ff
+//
 // My infinity pedal outputs to a file in /dev/usb hiddev0
 // This, however, could change (to say, hiddev1?)
-// Notes on fmt Formating, %b is base 2 %o is base 8.
-// Looks like the data is given in unsigned base 8 ints. uint8
-// From 24 byte buffer, bytes 5, 13, and 21 are important.
+//
 // Like the python code, use the linux program xte to simulate
 // keystrokes. For archlinux, this is found in the xautomation package.
 package main
 
-import "os"
-import "fmt"
-import "os/exec"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
 
 func main() {
 	file, err := os.Open("/dev/usb/hiddev0")
